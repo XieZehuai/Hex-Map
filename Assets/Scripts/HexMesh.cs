63605f292fa6f29e34ad13e56bdc711d906d36b6
XieZehuai <1544498268@qq.com>
2022-04-18 17:58:33 +0800

    Hex Map - Blending Cell Colors - 4 Fusing Edges

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -69,32 +69,42 @@ namespace HexMap
         private void Triangulate(HexDirection direction, HexCell cell)
         {
             // 把三角形看成由固定部分和过渡部分组成，固定部分为 中心 和 两条边上分割点 组成的三角形，
-            // 过渡部分为 两条边上分割点 和 两条边上的角 组成的四边形
+            // 过渡部分再分成中间的矩形部分和两边的三角形部分，中间的矩形用于和直接邻居过渡，两边的
+            // 三角形用于和直接邻居与间接邻居过渡
 
             // 生成三角形的固定部分
             Vector3 center = cell.transform.localPosition; // 单元格的中心
             Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
             Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
-            Color c1 = cell.color;
-            Color c2 = cell.color;
-
             AddTriangle(center, v1, v2);
-            AddTriangleColor(cell.color, c1, c2);
+            AddTriangleColor(cell.color);
 
-            // 生成三角形的过渡部分
-            Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
-            Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
+            // 生成三角形的矩形过渡部分
+            Vector3 bridge = HexMetrics.GetBridge(direction);
+            Vector3 v3 = v1 + bridge;
+            Vector3 v4 = v2 + bridge;
 
             // 获取当前方向上的三个邻居（一个正对当前方向，两个在当前方向的斜向上），
             // 让两个顶点的颜色与相邻单元格的颜色融合
-            HexCell previousNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
             HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
-            HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
-            Color c3 = (cell.color + previousNeighbor.color + neighbor.color) / 3f;
-            Color c4 = (cell.color + neighbor.color + nextNeighbor.color) / 3f;
+            Color bridgeColor = (cell.color + neighbor.color) * 0.5f;
 
             AddQuad(v1, v2, v3, v4);
-            AddQuadColor(c1, c2, c3, c4);
+            AddQuadColor(cell.color, bridgeColor);
+
+            // 生成三角形两个顶点上的小三角形过渡部分
+            Vector3 v5 = center + HexMetrics.GetFirstCorner(direction);
+            Vector3 v6 = center + HexMetrics.GetSecondCorner(direction);
+
+            HexCell previousNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
+            HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
+            Color c5 = (cell.color + previousNeighbor.color + neighbor.color) / 3f;
+            Color c6 = (cell.color + neighbor.color + nextNeighbor.color) / 3f;
+
+            AddTriangle(v1, v5, v3);
+            AddTriangleColor(cell.color, c5, bridgeColor);
+            AddTriangle(v2, v4, v6);
+            AddTriangleColor(cell.color, bridgeColor, c6);
         }
 
         /// <summary>
@@ -124,6 +134,16 @@ namespace HexMap
         }
 
         /// <summary>
+        /// 为当前三角形添加三个顶点的颜色
+        /// </summary>
+        private void AddTriangleColor(Color c1)
+        {
+            colors.Add(c1);
+            colors.Add(c1);
+            colors.Add(c1);
+        }
+
+        /// <summary>
         /// 使用给定的四个顶点生成一个四边形，四边形由两个三角形组成，顶点顺序分别为
         /// [v1, v3, v2] 和 [v2, v3, v4]，顶点顺序遵循左手定则
         /// </summary>
@@ -145,7 +165,7 @@ namespace HexMap
         }
 
         /// <summary>
-        /// 为当前四边形添加顶点的颜色，四边形由两个三角形组成，顶点顺序分别为
+        /// 为当前四边形添加顶点的颜色，四边形由两个三角形组成，颜色顺序分别为
         /// [c1, c3, c2] 和 [c2, c3, c4]，顶点顺序遵循左手定则
         /// </summary>
         private void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
@@ -154,6 +174,18 @@ namespace HexMap
             colors.Add(c2);
             colors.Add(c3);
             colors.Add(c4);
+        }
+
+        /// <summary>
+        /// 为当前四边形添加顶点的颜色，四边形由两个三角形组成，颜色顺序分别为
+        /// [c1, c2, c1] 和 [c1, c2, c2]，顶点顺序遵循左手定则
+        /// </summary>
+        private void AddQuadColor(Color c1, Color c2)
+        {
+            colors.Add(c1);
+            colors.Add(c1);
+            colors.Add(c2);
+            colors.Add(c2);
         }
     }
 }
--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -73,5 +73,10 @@ namespace HexMap
         {
             return corners[(int)direction + 1] * solidFactor;
         }
+
+        public static Vector3 GetBridge(HexDirection direction)
+        {
+            return (corners[(int)direction] + corners[(int)direction + 1]) * 0.5f * blendFactor;
+        }
     }
 }
